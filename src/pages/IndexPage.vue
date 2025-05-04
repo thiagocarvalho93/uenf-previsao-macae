b4a7c2c60883f9fdbf2203eaf836824ca50f7986
thiagocarvalho93 <thiagopaes1993@gmail.com>
2025-05-04 16:10:56 -0300

    🚧 Reformulando layout

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,21 +1,36 @@
 <template>
   <q-page class="q-pa-md">
-    <q-card flat bordered class="q-pa-md q-mb-md fade">
-      <div class="row q-col-gutter-md">
-        <div class="col-12 col-sm-4 col-md-2 fade">
+    <q-card flat bordered class="q-pa-sm">
+      <div class="row q-col-gutter-md flex-center q-mb-sm">
+        <!-- <div class="col-12 col-sm-4 col-md-2 fade">
           <q-select v-model="variable" :options="variables" label="Variável" />
+        </div> -->
+        <div class="">
+          <q-icon
+            @click="handlePrevious"
+            :class="previousDisabled ? 'disabled' : ''"
+            class="cursor-pointer"
+            name="chevron_left"
+            size="xl"
+          />
         </div>
-        <div class="col-12 col-sm-4 col-md-2 fade">
+        <div class="">
           <q-select v-model="selectedDate" :options="dates" label="Data" />
         </div>
-        <div class="col-12 col-sm-2 col-md-1 fade">
+        <div class="">
           <q-select v-model="runHour" :options="hours" label="Hora" />
         </div>
+        <div class="">
+          <q-icon
+            @click="handleNext"
+            :class="nextDisabled ? 'disabled' : ''"
+            class="cursor-pointer"
+            name="chevron_right"
+            size="xl"
+          />
+        </div>
       </div>
-    </q-card>
-
-    <q-card v-touch-swipe.mouse="handleSwipe" flat bordered class="fade">
-      <q-img :src="imageUrl" alt="Previsão" />
+      <q-img v-touch-swipe.mouse="handleSwipe" :src="imageUrl" alt="Previsão" />
     </q-card>
     <div class="q-my-md row flex-center col-12"></div>
   </q-page>
@@ -29,7 +44,7 @@ const variable = ref('Temperatura')
 const selectedDate = ref('2025-05-03')
 const runHour = ref('00Z')
 
-const variables = ['Temperatura', 'Vento', 'Pressao', 'Precipitacao', 'Radiacao']
+// const variables = ['Temperatura', 'Vento', 'Pressao', 'Precipitacao', 'Radiacao']
 const dates = ['2025-05-03', '2025-05-04', '2025-05-05']
 const hours = ['00Z', '03Z', '06Z', '09Z', '12Z', '15Z', '18Z', '21Z']
 
@@ -38,17 +53,55 @@ const imageUrl = computed(
     `${BUCKET_URL}/${variable.value}/${FILE_PREFIX_MAP.get(variable.value)}_g1_${selectedDate.value.replaceAll('-', '')}_${runHour.value}.png`,
 )
 
+const hourIndex = computed(() => hours.findIndex((x) => x == runHour.value))
+const dateIndex = computed(() => dates.findIndex((x) => x == selectedDate.value))
+
+const previousDisabled = computed(() => hourIndex.value == 0 && dateIndex.value == 0)
+const nextDisabled = computed(
+  () => hourIndex.value + 1 >= hours.length && dateIndex.value + 1 >= dates.length,
+)
+
 function handleSwipe(e) {
-  const indiceHora = hours.findIndex((x) => x == runHour.value)
+  console.log('swiped')
 
   if (e.direction == 'left') {
-    const proximoIndice = indiceHora + 1 >= hours.length ? indiceHora : indiceHora + 1
-    runHour.value = hours[proximoIndice]
+    handleNext()
   }
 
   if (e.direction == 'right') {
-    const proximoIndice = indiceHora - 1 < 0 ? indiceHora : indiceHora - 1
-    runHour.value = hours[proximoIndice]
+    handlePrevious()
+  }
+}
+
+function handlePrevious() {
+  if (previousDisabled.value) return
+
+  const isFirstHour = hourIndex.value == 0
+  const isFirstDay = dateIndex.value == 0
+
+  if (isFirstHour) {
+    const nextHourIndex = isFirstDay ? hourIndex : hours.length - 1
+    const nextDateIndex = isFirstDay ? dateIndex : dateIndex.value - 1
+
+    runHour.value = hours[nextHourIndex]
+    selectedDate.value = dates[nextDateIndex]
+  } else {
+    runHour.value = hours[hourIndex.value - 1]
+  }
+}
+
+function handleNext() {
+  if (nextDisabled.value) return
+  const isLastHour = hourIndex.value + 1 >= hours.length
+  const isLastDate = dateIndex.value + 1 >= dates.length
+
+  if (isLastHour) {
+    const nextHourIndex = isLastDate ? hourIndex.value : 0
+    const nextDateIndex = isLastDate ? dateIndex.value : dateIndex.value + 1
+    runHour.value = hours[nextHourIndex]
+    selectedDate.value = dates[nextDateIndex]
+  } else {
+    runHour.value = hours[hourIndex.value + 1]
   }
 }
 </script>
