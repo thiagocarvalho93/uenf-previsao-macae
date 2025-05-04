fe11e37a66569c817f05d57d981253879359317b
thiagocarvalho93 <thiagopaes1993@gmail.com>
2025-05-04 19:28:51 -0300

    🚧 Adicionando botão de mudança de variável

--- src/helpers/url-helper.js
+++ src/helpers/url-helper.js
@@ -1,5 +1,5 @@
 import { BUCKET_URL, FILE_PREFIX_MAP } from 'src/constants/constants'
 
-export const getImageUrl = (variable, formattedDate, formattedHour) => {
-  return `${BUCKET_URL}/${variable}/${FILE_PREFIX_MAP.get(variable)}_g1_${formattedDate}_${formattedHour}.png`
+export const getImageUrl = (variable, formattedDate, formattedHour, grid) => {
+  return `${BUCKET_URL}/${variable}/${FILE_PREFIX_MAP.get(variable)}_${grid}_${formattedDate}_${formattedHour}.png`
 }
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,68 +1,93 @@
 <template>
   <q-page class="q-pa-md">
     <q-card flat bordered class="q-pa-sm">
+      <!-- <div class="row q-row-gutter-md flex-center q-mb-md">
+        <div v-for="item in GRIDS" :key="item">
+          <q-radio size="sm" v-model="selectedGrid" :val="item" :label="item" />
+        </div>
+      </div> -->
       <div class="row q-col-gutter-md flex-center q-mb-sm">
-        <!-- <div class="col-12 col-sm-4 col-md-2 fade">
-          <q-select v-model="variable" :options="variables" label="Variável" />
-        </div> -->
-        <div class="">
-          <q-icon
-            @click="handlePrevious"
-            :class="previousDisabled ? 'disabled' : ''"
-            class="cursor-pointer"
-            name="chevron_left"
-            size="xl"
-          />
-        </div>
-        <div class="">
-          <q-select v-model="selectedDate" :options="dates" label="Data" />
-        </div>
-        <div class="">
-          <q-select v-model="runHour" :options="HOURS" label="Hora" />
-        </div>
-        <div class="">
-          <q-icon
-            @click="handleNext"
-            :class="nextDisabled ? 'disabled' : ''"
-            class="cursor-pointer"
-            name="chevron_right"
-            size="xl"
-          />
-        </div>
+        <q-icon
+          @click="handlePrevious"
+          :class="previousDisabled ? 'disabled' : ''"
+          class="cursor-pointer"
+          name="chevron_left"
+          size="xl"
+        />
+        <q-select dense v-model="selectedDate" :options="dates" label="Data" />
+        <q-select dense v-model="selectedHour" :options="hours" label="Hora" />
+        <q-icon
+          @click="handleNext"
+          :class="nextDisabled ? 'disabled' : ''"
+          class="cursor-pointer"
+          name="chevron_right"
+          size="xl"
+        />
       </div>
       <q-img v-touch-swipe.mouse="handleSwipe" :src="imageUrl" alt="Previsão" />
     </q-card>
-    <div class="q-my-md row flex-center col-12"></div>
+
+    <q-page-sticky position="bottom-right" :offset="[30, 30]">
+      <q-fab hide-icon label="Variável" direction="up" color="primary">
+        <q-fab-action
+          v-for="item in VARIABLES"
+          :key="item"
+          @click="handleChangeVariable(item)"
+          color="accent"
+          hide-icon
+          :label="item"
+        />
+      </q-fab>
+    </q-page-sticky>
   </q-page>
 </template>
 
 <script setup>
 import { ref, computed, onMounted } from 'vue'
-import { HOURS, VARIABLES } from 'src/constants/constants'
+import { GRIDS, HOURS, HOURS_RADIATION, VARIABLES } from 'src/constants/constants'
 import { getImageUrl } from 'src/helpers/url-helper'
 import { api } from 'src/boot/axios'
 import { formatDateNumbersOnly, generateDatesArray } from 'src/helpers/date-helper'
 
 // Reactive state
-const variable = ref(VARIABLES[0])
+const selectedVariable = ref(VARIABLES[0])
 const selectedDate = ref('')
-const runHour = ref(HOURS[0])
+const selectedHour = ref(HOURS[0])
+const selectedGrid = ref(GRIDS[0])
 const dates = ref([])
+const hours = ref(HOURS)
 
 // Computed indices
-const hourIndex = computed(() => HOURS.indexOf(runHour.value))
+const hourIndex = computed(() => hours.value.indexOf(selectedHour.value))
 const dateIndex = computed(() => dates.value.indexOf(selectedDate.value))
 
 // Image URL builder
 const imageUrl = computed(() =>
-  getImageUrl(variable.value, formatDateNumbersOnly(selectedDate.value), runHour.value),
+  getImageUrl(
+    selectedVariable.value,
+    formatDateNumbersOnly(selectedDate.value),
+    selectedHour.value,
+    selectedGrid.value,
+  ),
 )
 
 // Navigation state
 const previousDisabled = computed(() => hourIndex.value === 0 && dateIndex.value === 0)
 const nextDisabled = computed(
-  () => hourIndex.value === HOURS.length - 1 && dateIndex.value === dates.value.length - 1,
+  () => hourIndex.value === hours.value.length - 1 && dateIndex.value === dates.value.length - 1,
 )
+
+function handleChangeVariable(item) {
+  if (item == 'Radiacao') {
+    hours.value = HOURS_RADIATION
+    selectedHour.value = HOURS_RADIATION.includes(selectedHour.value)
+      ? selectedHour.value
+      : HOURS_RADIATION[0]
+  } else {
+    hours.value = HOURS
+  }
+  selectedVariable.value = item
+}
 
 // Swipe handler
 function handleSwipe(e) {
@@ -75,21 +100,21 @@ function handlePrevious() {
   if (previousDisabled.value) return
 
   if (hourIndex.value === 0) {
-    runHour.value = HOURS[HOURS.length - 1]
+    selectedHour.value = hours.value[hours.value.length - 1]
     selectedDate.value = dates.value[dateIndex.value - 1]
   } else {
-    runHour.value = HOURS[hourIndex.value - 1]
+    selectedHour.value = hours.value[hourIndex.value - 1]
   }
 }
 
 function handleNext() {
   if (nextDisabled.value) return
 
-  if (hourIndex.value === HOURS.length - 1) {
-    runHour.value = HOURS[0]
+  if (hourIndex.value === hours.value.length - 1) {
+    selectedHour.value = hours.value[0]
     selectedDate.value = dates.value[dateIndex.value + 1]
   } else {
-    runHour.value = HOURS[hourIndex.value + 1]
+    selectedHour.value = hours.value[hourIndex.value + 1]
   }
 }
 
